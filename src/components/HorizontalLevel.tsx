e16224570169440144655e50e3b7bee85b2e40c8
Christian Albert <calbert1209@gmail.com>
2023-05-07 23:00:44 +0900

    :lipstick: styles for HorizontalLevel

--- src/components/HorizontalLevel.tsx
+++ src/components/HorizontalLevel.tsx
@@ -1,60 +1,32 @@
 import { useMemo } from "preact/hooks";
 import { TidalEvent } from "../services/data-fetch";
+import "./HorizontalLevel.css";
 
 type Props = TidalEvent;
 
 const getTimeMark = (isoDateString: string) => {
-  const [_, timePart] = isoDateString.split('T');
+  const [_, timePart] = isoDateString.split("T");
   return timePart.substring(0, 5);
-}
+};
 
 export const HorizontalLevel = ({ level, type, timeStamp }: Props) => {
-  const color = type === 'high' ? 'mediumblue' : 'green';
+  const color = type === "high" ? "mediumblue" : "green";
   const timeMark = useMemo(() => getTimeMark(timeStamp), [timeStamp]);
 
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          flexGrow: 1,
-        }}
-      />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-end",
-          alignItems: "flex-end",
-          color: `${color}`,
-          fontSize: "0.8em",
-          lineHeight: `1em`,
-        }}
-      >
+    <div className="horizontal-level_outer">
+      <div className="horizontal-level_upper" />
+      <div className="horizontal-level_middle" style={{ color: `${color}` }}>
         <div
-          style={{
-            flexGrow: 1,
-            height: "1em",
-            borderBottom: `1px dotted ${color}`,
-          }}
+          className="horizontal-level_middle_line"
+          style={{ borderBottomColor: color }}
         />
-        <div style={{ marginLeft: "4px", textShadow: '0 0 2px white' }}>{level} cm</div>
-        <div style={{ marginLeft: "4px", textShadow: '0 0 2px white' }}>{timeMark}</div>
+        <div className="horizontal-level_label">{level} cm</div>
+        <div className="horizontal-level_label">{timeMark}</div>
       </div>
       <div
-        style={{
-          width: "100%",
-          height: `${level * 2}px`,
-        }}
+        className="horizontal-level_lower"
+        style={{ height: `${level * 2}px` }}
       />
     </div>
   );
