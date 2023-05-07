9b53d83e64c5455a96f32db2d4231f3d41fd1124
Christian Albert <calbert1209@gmail.com>
2023-05-07 23:07:03 +0900

    :lipstick: add styles for VerticalMarker

--- src/components/VerticalMarker.tsx
+++ src/components/VerticalMarker.tsx
@@ -1,34 +1,15 @@
+import "./VerticalMarker.css";
 
 export const VerticalMarker = () => {
   const [hours, minutes] = [11, 30];
-  const timeInMinutes = (hours * 60) + minutes;
+  const timeInMinutes = hours * 60 + minutes;
 
-  const percent = timeInMinutes / 60 / 24 * 100;
+  const percent = (timeInMinutes / 60 / 24) * 100;
 
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        display: "flex",
-      }}
-    >
-      <div
-        style={{
-          height:'100%',
-          width: `${percent}%`,
-          borderRight: '1px solid red',
-        }}
-      />
-      <div
-        style={{
-          height:'100%',
-          flexGrow: 1,
-        }}
-      />
+    <div className="vertical-marker_outer">
+      <div className="vertical-marker_left" style={{ width: `${percent}%` }} />
+      <div className="vertical-marker_right" />
     </div>
   );
 };
