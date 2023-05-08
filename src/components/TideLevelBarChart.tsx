a89e5dc03050df1eb1b2ec7662e09d4cf4c78d22
Christian Albert <calbert1209@gmail.com>
2023-05-08 22:13:19 +0900

    :bug: ensure negative bars are drawn

--- src/components/TideLevelBarChart.tsx
+++ src/components/TideLevelBarChart.tsx
@@ -1,22 +1,29 @@
 import { ComponentChildren } from "preact";
 import "./TideLevelBarChart.css";
 
+const kScaleY = 2;
+
 type LevelBarProps = {
   level: number;
   color: string;
 };
 
-export const TideLevelBar = ({ level, color }: LevelBarProps) => (
-  <li
-    className="tide-level-bar"
-    style={{
-      height: `${level * 2}px`,
-      backgroundColor: color,
-    }}
-  >
-    <div className="tide-level-bar_label">{level}</div>
-  </li>
-);
+export const TideLevelBar = ({ level, color }: LevelBarProps) => {
+  const height = Math.abs(level);
+  const shift = Math.min(0, level);
+  return (
+    <li
+      className="tide-level-bar"
+      style={{
+        height: `${height * kScaleY}px`,
+        backgroundColor: color,
+        transform: `translateY(${shift * kScaleY * -1}px)`
+      }}
+    >
+      <div className="tide-level-bar_label">{level}</div>
+    </li>
+  );
+}
 
 type ChartProps = {
   children: ComponentChildren;
