a64b606d11985493453887d6c5cfb9de669b57f9
Christian Albert <calbert1209@gmail.com>
2023-05-31 21:47:31 +0900

    :recycle: convert tidal chart to svg

--- src/components/TideLevelWindow/TideLevelBarChart.tsx
+++ src/components/TideLevelWindow/TideLevelBarChart.tsx
@@ -1,34 +1,33 @@
-import { ComponentChildren } from "preact";
-import "./TideLevelBarChart.css";
+import { TidalEvent } from "../../services/data-fetch";
+import { colorRange } from "../../ui";
 
 const kScaleY = 2;
 
-type LevelBarProps = {
-  level: number;
-  color: string;
+type Props = {
+  events: TidalEvent[];
+  reverseIndex: Record<number, number>;
 };
 
-export const TideLevelBar = ({ level, color }: LevelBarProps) => {
-  const height = Math.abs(level);
-  const shift = Math.min(0, level);
+export const TideLevelBarChart = ({ events, reverseIndex }: Props) => {
+  const chartWidth = Math.min(window.innerWidth * 0.8, 504);
+  const barWidth = (chartWidth - 23 * 1) / 24;
+
   return (
-    <li
-      className="tide-level-bar"
-      style={{
-        height: `${height * kScaleY}px`,
-        backgroundColor: color,
-        transform: `translateY(${shift * kScaleY * -1}px)`
-      }}
+    <svg
+      viewBox={`0 0 ${chartWidth} 300`}
+      xmlns="http://www.w3.org/2000/svg"
+      width={chartWidth}
     >
-      <div className="tide-level-bar_label">{level}</div>
-    </li>
+      {events.map(({ level, timeStamp }, i) => (
+        <rect
+          key={timeStamp}
+          x={i * (barWidth + 1)}
+          y={level >= 0 ? 250 - level * kScaleY : 250}
+          width={barWidth}
+          height={Math.abs(level * kScaleY)}
+          fill={colorRange[reverseIndex[level]]}
+        />
+      ))}
+    </svg>
   );
-}
-
-type ChartProps = {
-  children: ComponentChildren;
 };
-
-export const TideLevelBarChart = ({ children }: ChartProps) => (
-  <ul className="tide-level-bar-chart">{children}</ul>
-);
--- src/components/TideLevelWindow/TideLevelWindow.tsx
+++ src/components/TideLevelWindow/TideLevelWindow.tsx
@@ -1,8 +1,7 @@
 import { TidalChart } from "../../services/data-fetch";
 import { RenderConditionally } from "../../ui/components/RenderConditionally";
 import { VerticalMarker } from "./../VerticalMarker";
-import { TideLevelBar, TideLevelBarChart } from "./TideLevelBarChart";
-import { colorRange } from "../../ui";
+import { TideLevelBarChart } from "./TideLevelBarChart";
 import { useTideLevelWindowState } from "./useTideLevelWindowState";
 import { Row, PositionRelative } from "../../ui/components";
 import { ExtremityEventList } from "./ExtremityEvent";
@@ -27,15 +26,10 @@ export const TideLevelWindow = ({
       <RenderConditionally when={!!hourlyEvents.length}>
         <Row className="tide-level-window_row" gap={"32px"} align="flex-end">
           <PositionRelative>
-            <TideLevelBarChart>
-              {hourlyEvents.map(({ level, timeStamp }) => (
-                <TideLevelBar
-                  key={timeStamp}
-                  level={level}
-                  color={colorRange[reverseIndex[level]]}
-                />
-              ))}
-            </TideLevelBarChart>
+            <TideLevelBarChart
+              events={hourlyEvents}
+              reverseIndex={reverseIndex}
+            />
             <RenderConditionally when={isTodaySelected}>
               <VerticalMarker />
             </RenderConditionally>
